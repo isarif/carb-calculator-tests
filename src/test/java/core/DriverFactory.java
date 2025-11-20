5ebcc50003b036c33ae3380e7475ec2d6036ee61
Irteza Arif <irteza.saqlaindin.arif@gmail.com>
2025-11-19 19:48:20 -0500

    Use headless Chrome for CI

--- src/test/java/core/DriverFactory.java
+++ src/test/java/core/DriverFactory.java
@@ -3,6 +3,7 @@ package core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -17,7 +18,14 @@ public class DriverFactory {
 
     private static void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        // Headless mode for CI
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
