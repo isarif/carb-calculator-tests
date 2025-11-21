d45f9e1a63f9ed89f594cc3f3d5466159683ee85
Irteza Arif <irteza.saqlaindin.arif@gmail.com>
2025-11-20 20:35:36 -0500

    Add automation run demo GIF

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -3,13 +3,12 @@ package hooks;
 import core.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
 
 public class Hooks {
 
     @Before
     public void beforeScenario() {
-        WebDriver driver = DriverFactory.getDriver();
+        DriverFactory.getDriver();
     }
 
     @After
